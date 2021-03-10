5b7cad440f7cb19b8479d365f8291b8b3c004501
mhamzic <mhamzic@gmail.com>
2021-03-10 10:15:34 +0100

    Commenting

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  // Only change code below this line
+  // This is single line commment
+  /* This is block comment that
+    spans multiple lines*/
+  // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
